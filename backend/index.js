c12a1d205b59db4c4f378d0fc29183108966e8da
AFROUNE MOHAMED ADAM <148912667+adam1508042001@users.noreply.github.com>
2024-11-28 10:48:21 +0100

    routes get livres dispo, historique empreinte

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,18 @@ app.listen(8080, () => {  console.log('Serveur bien en écoute')})
 //le (req, res) => { res.send("Liste des parkings") } est un callback (une fonction exécutées chaque fois qu'une requete get est reçue pour ce chemin)
 //req pour requette : ce sont les infos envoyées par le user , parametres ... 
 //res reponse : configure et envoie la reponse au client 
- 
-app.get('/livres', (req,res) => {    res.status(200).json(livres); });
+
+
+
+// //route get get tout les livres
+// app.get('/livres', (req,res) => { 
+//     const livresDispos = livres.livres.filter(livre => livre.disponible);
+//     res.status(200).json(livresDispos);
+
+//      });
+
+// Consulter l'historique des emprunts
+app.get('/historique', (req, res) => {
+    res.status(200).json(livres.historiqueEmprunts);
+});
 
