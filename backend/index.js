43cadc5f40d72f454ced232696a98a4ad0723902
AFROUNE MOHAMED ADAM <148912667+adam1508042001@users.noreply.github.com>
2024-11-28 10:29:03 +0100

    test routes

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,16 @@ const cors = require('cors');
 app.use(cors());
 
 
-
-app.listen(8080, () => {  console.log('Serveur en écoute')})
-
+app.listen(8080, () => {  console.log('Serveur bien en écoute')})
 
 
+
+
+
+//definir une route http pour un  requete get
+//le (req, res) => { res.send("Liste des parkings") } est un callback (une fonction exécutées chaque fois qu'une requete get est reçue pour ce chemin)
+//req pour requette : ce sont les infos envoyées par le user , parametres ... 
+//res reponse : configure et envoie la reponse au client 
+ 
+app.get('/livres', (req,res) => {    res.send(" Liste des livres")})
 
