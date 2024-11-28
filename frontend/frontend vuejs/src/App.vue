d45f8a2d98da0e1c81c748415edc21fbe964adcc
AFROUNE MOHAMED ADAM <148912667+adam1508042001@users.noreply.github.com>
2024-11-28 10:12:57 +0100

    configuration de frontend vue js et le backend node express

---
+++ frontend/frontend vuejs/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App)
+
+
+</script>
+
+<template>
+
+ <div>
+  hello
+ </div>
+
+ 
+</template>
+
+<style scoped>
+
+
+</style>
